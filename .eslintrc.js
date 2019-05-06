b078c7287f398ec967976f2ff8177a118f209417
Keith River <keith.river@gmail.com>
2019-05-07 00:56:09 +0300

    updated linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,3 @@
 const config = require("@spellbook/linter-config");
 
-config.rules["unicorn/prevent-abbreviations"][1].replacements["dev"] = false;
-delete config.parserOptions.babelOptions;
-
 module.exports = config;
